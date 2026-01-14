42c9adf73a01dab1d408025e675811e334ee75f8
Ahmed <ahmedalwanmunshed@gmail.com>
2026-01-14 10:51:39 +0300

    Update database.py

--- numrun/database.py
+++ numrun/database.py
@@ -16,47 +16,42 @@ class Database:
                     command TEXT NOT NULL,
                     tags TEXT,
                     usage_count INTEGER DEFAULT 0,
-                    last_used TEXT
+                    last_used TEXT,
+                    alias TEXT UNIQUE
                 )
             """)
+            cursor = self.conn.execute("PRAGMA table_info(commands)")
+            columns = [column[1] for column in cursor.fetchall()]
+            if 'alias' not in columns:
+                self.conn.execute("ALTER TABLE commands ADD COLUMN alias TEXT")
 
-    def add_command(self, command, num=None):
+    def add_command(self, command, alias_name=None):
         with self.conn:
-            if num:
-                self.conn.execute("INSERT OR REPLACE INTO commands (cmd_number, command) VALUES (?, ?)", (num, command))
-                return num
-            else:
-                cursor = self.conn.execute("INSERT INTO commands (command) VALUES (?)", (command,))
-                return cursor.lastrowid
+            cursor = self.conn.execute(
+                "INSERT INTO commands (command, alias) VALUES (?, ?)", 
+                (command, alias_name)
+            )
+            return cursor.lastrowid
+
+    def is_alias_exists(self, alias_name):
+        if not alias_name: return False
+        res = self.conn.execute("SELECT 1 FROM commands WHERE alias = ?", (alias_name,)).fetchone()
+        return res is not None
 
     def get_all(self):
-        return self.conn.execute("SELECT * FROM commands ORDER BY cmd_number").fetchall()
+        return self.conn.execute("SELECT cmd_number, command, alias, usage_count FROM commands ORDER BY cmd_number").fetchall()
 
     def get_by_num(self, num):
-        res = self.conn.execute("SELECT command FROM commands WHERE cmd_number = ?", (num,)).fetchone()
-        return res
+        return self.conn.execute("SELECT command FROM commands WHERE cmd_number = ?", (num,)).fetchone()
+
+    def get_by_alias(self, alias_name):
+        return self.conn.execute("SELECT command, cmd_number FROM commands WHERE alias = ?", (alias_name,)).fetchone()
 
     def increment_usage(self, num):
         now = datetime.now().strftime("%Y-%m-%d %H:%M")
         with self.conn:
             self.conn.execute("UPDATE commands SET usage_count = usage_count + 1, last_used = ? WHERE cmd_number = ?", (now, num))
 
-    def update_command(self, num, new_cmd):
-        with self.conn:
-            self.conn.execute("UPDATE commands SET command = ? WHERE cmd_number = ?", (new_cmd, num))
-
-    def add_tag(self, num, tag):
-        with self.conn:
-            res = self.conn.execute("SELECT tags FROM commands WHERE cmd_number = ?", (num,)).fetchone()
-            current = res[0] if res and res[0] else ""
-            tags = set(current.split(",")) if current else set()
-            tags.add(tag)
-            self.conn.execute("UPDATE commands SET tags = ? WHERE cmd_number = ?", (",".join(filter(None, tags)), num))
-
     def delete(self, num):
         with self.conn:
             self.conn.execute("DELETE FROM commands WHERE cmd_number = ?", (num,))
-
-    def search(self, query):
-        q = f"%{query}%"
-        return self.conn.execute("SELECT * FROM commands WHERE command LIKE ? OR tags LIKE ?", (q, q)).fetchall()
