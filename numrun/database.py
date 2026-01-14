c78a9c0812a7da7c355032853135c0ca3a71da19
Ahmed <ahmedalwanmunshed@gmail.com>
2026-01-14 11:23:07 +0300

    Update database.py

--- numrun/database.py
+++ numrun/database.py
@@ -14,40 +14,49 @@ class Database:
                 CREATE TABLE IF NOT EXISTS commands (
                     cmd_number INTEGER PRIMARY KEY AUTOINCREMENT,
                     command TEXT NOT NULL,
-                    tags TEXT,
+                    group_name TEXT DEFAULT 'general',
                     usage_count INTEGER DEFAULT 0,
                     last_used TEXT,
                     alias TEXT UNIQUE
                 )
             """)
+            # التحقق من تحديث الأعمدة في النسخ القديمة
             cursor = self.conn.execute("PRAGMA table_info(commands)")
-            columns = [column[1] for column in cursor.fetchall()]
-            if 'last_used' not in columns:
-                self.conn.execute("ALTER TABLE commands ADD COLUMN last_used TEXT")
-            if 'alias' not in columns:
+            cols = [c[1] for c in cursor.fetchall()]
+            if 'group_name' not in cols: 
+                self.conn.execute("ALTER TABLE commands ADD COLUMN group_name TEXT DEFAULT 'general'")
+            if 'alias' not in cols: 
                 self.conn.execute("ALTER TABLE commands ADD COLUMN alias TEXT")
 
-    def add_command(self, command, alias_name=None):
+    def add_command(self, command, alias=None, group='general'):
         with self.conn:
             cursor = self.conn.execute(
-                "INSERT INTO commands (command, alias) VALUES (?, ?)", 
-                (command, alias_name)
+                "INSERT INTO commands (command, alias, group_name) VALUES (?, ?, ?)", 
+                (command, alias, group)
             )
             return cursor.lastrowid
 
-    def is_alias_exists(self, alias_name):
-        if not alias_name: return False
-        res = self.conn.execute("SELECT 1 FROM commands WHERE alias = ?", (alias_name,)).fetchone()
-        return res is not None
+    def update_command(self, num, new_cmd, new_alias):
+        with self.conn:
+            self.conn.execute("UPDATE commands SET command = ?, alias = ? WHERE cmd_number = ?", 
+                             (new_cmd, new_alias, num))
 
-    def get_all(self):
-        return self.conn.execute("SELECT cmd_number, command, alias, usage_count, last_used FROM commands ORDER BY cmd_number").fetchall()
+    def is_alias_exists(self, alias):
+        if not alias: return False
+        return self.conn.execute("SELECT 1 FROM commands WHERE alias = ?", (alias,)).fetchone() is not None
 
-    def get_by_num(self, num):
-        return self.conn.execute("SELECT command FROM commands WHERE cmd_number = ?", (num,)).fetchone()
+    def get_all(self, group=None):
+        # تحديد الأعمدة بالاسم لضمان الترتيب الصحيح في الـ CLI
+        query = "SELECT cmd_number, command, group_name, usage_count, last_used, alias FROM commands"
+        if group:
+            return self.conn.execute(query + " WHERE group_name = ?", (group,)).fetchall()
+        return self.conn.execute(query + " ORDER BY cmd_number").fetchall()
 
-    def get_by_alias(self, alias_name):
-        return self.conn.execute("SELECT command, cmd_number FROM commands WHERE alias = ?", (alias_name,)).fetchone()
+    def get_by_id_or_alias(self, identifier):
+        res = self.conn.execute("SELECT command, cmd_number, alias FROM commands WHERE alias = ?", (identifier,)).fetchone()
+        if not res and str(identifier).isdigit():
+            res = self.conn.execute("SELECT command, cmd_number, alias FROM commands WHERE cmd_number = ?", (identifier,)).fetchone()
+        return res
 
     def increment_usage(self, num):
         now = datetime.now().strftime("%Y-%m-%d %H:%M")
@@ -55,13 +64,5 @@ class Database:
             self.conn.execute("UPDATE commands SET usage_count = usage_count + 1, last_used = ? WHERE cmd_number = ?", (now, num))
 
     def delete(self, num):
-        with self.conn:
+        with self.conn: 
             self.conn.execute("DELETE FROM commands WHERE cmd_number = ?", (num,))
-
-    def set_alias(self, num, alias_name):
-        with self.conn:
-            try:
-                self.conn.execute("UPDATE commands SET alias = ? WHERE cmd_number = ?", (alias_name, num))
-                return True
-            except sqlite3.IntegrityError:
-                return False
